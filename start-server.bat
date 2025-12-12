99c063584a57baf7f9ed8fea71ab4135b26a23dd
ijrdevjunior <inaldo_2005@icloud.com>
2025-12-11 22:17:50 -0500

    Add files via upload
    server update 002

---
+++ start-server.bat
@@ -0,0 +1,9 @@
+@echo off
+set SUPABASE_URL=https://cjwskbhnbppouwfxjezf.supabase.co
+set SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqd3NrYmhuYnBwb3V3ZnhqZXpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0OTE3NTQsImV4cCI6MjA4MTA2Nzc1NH0.Hsz8lobXBSVLNL5sn0WQYNouwer-mOYFTsjxmvIIuAU
+set OWNER_PASSWORD=gol2024
+echo.
+echo Iniciando servidor com Supabase...
+echo.
+node server.js
+pause
